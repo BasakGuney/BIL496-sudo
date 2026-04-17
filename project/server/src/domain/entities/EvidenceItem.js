48c067ea88e285eb0cf66cb911ea7d40f22a4bb1
inciskaleli <incisila_kaleli@hotmail.com>
2026-04-17 05:02:54 +0300

    chore: remove unused client and server dead code

--- project/server/src/domain/entities/EvidenceItem.js
+++
@@ -1,7 +0,0 @@
-export class EvidenceItem {
-  constructor({ ref, claim, timestampSec }) {
-    this.ref = String(ref || "");
-    this.claim = String(claim || "");
-    this.timestampSec = Number(timestampSec || 0);
-  }
-}
